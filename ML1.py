6ccc73dfbd50816dd6681e645da090e256b5b1ad
Hades-07 <72396811+Hades-07@users.noreply.github.com>
2022-11-18 00:06:02 +0530

    Update ML1.py

--- ML1.py
+++ ML1.py
@@ -10,8 +10,6 @@ import matplotlib.pyplot as plt
 # nltk
 from nltk.stem import WordNetLemmatizer
 # sklearn
-from sklearn.svm import LinearSVC
-from sklearn.naive_bayes import BernoulliNB
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
